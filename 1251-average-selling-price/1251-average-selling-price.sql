584eacb94b7266f69d4d88b9da2c5b70514a1580
Helly modi <190320116021.it.helly.modi@gmail.com>
2024-05-26 20:06:33 +0530

    Time: 1587 ms (42.86%), Space: 0B (100.00%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,9 @@
+SELECT
+p.product_id,
+IFNULL(ROUND(SUM(price * units) / SUM(units), 2), 0) AS average_price
+FROM
+Prices AS p
+ left JOIN UnitsSold AS u
+ON p.product_id = u.product_id AND purchase_date BETWEEN start_date AND end_date
+GROUP BY 1;
+
