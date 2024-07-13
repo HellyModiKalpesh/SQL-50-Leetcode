fa3ddc0dad663453194c332cc0a337b75c4e6083
Helly modi <190320116021.it.helly.modi@gmail.com>
2024-07-13 11:37:01 +0530

    Time: 280 ms (97.32%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select
+case
+    when id%2 =1 and id not in (select max(id) from Seat) then id+1
+    when id%2=0 then id-1
+    else id
+end as id,
+student from Seat
+order by id 
